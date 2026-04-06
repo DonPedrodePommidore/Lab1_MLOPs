0895eae980f7dd8ce65b3d237689450cf4a4e715
DonPedrodePommidore <mborek23@gmail.com>
2026-04-06 09:33:36 +0200

    Secrets management

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from lab1-mlops!")
+import os
+import yaml
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
 
+
+def export_envs(environment: str = "dev") -> None:
+    config_dir = "config"
+
+    env = os.path.join(config_dir, f".env.{environment}")
+    if not os.path.exists(env):
+        raise FileNotFoundError(f"{env} nie istnieje")
+    load_dotenv(dotenv_path=env, override=True)
+
+    if os.path.exists("secrets.yaml"):
+        with open("secrets.yaml", "r") as f:
+            for key, value in yaml.safe_load(f).items():
+                os.environ[key] = str(value)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Load environment variables from specified.env file.")
+    parser.add_argument("--environment", type=str, default="dev", help="The environment to load (dev, test, prod)")
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
+    print("SECRET: ", settings.SECRETS)
